97b27b468d80bf356660d39f7807782d692b1861
Matteo Canzari <matteo.canzari@gmail.com>
2020-04-07 13:04:00 +0200

    added header

---
+++ components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+    return (
+        <View style = {styles.header}>
+            <Text style = {styles.title}>INAF Covid-19 Tracking</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        paddingTop: 38,
+        backgroundColor: '#1e90ff'
+    },
+    title: {
+        fontSize: 24,
+        textAlign: "center", 
+        color: '#fff',
+        fontWeight: 'bold',
+    }
+});
